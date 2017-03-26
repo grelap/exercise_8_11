15ae51889349ea276a755affedbf3d922fb7428f
xenogonx <pablez@wp.pl>
2017-03-26 19:51:52 +0200

    events

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+var list = document.getElementById('list');
+var add = document.getElementById('addElem')
+
+add.addEventListener('click', function() {
+	list.innerHTML += '<li>item ' + document.getElementsByTagName('li').length + '</li>'   
+});
+
+
+
+
+
+
+
+
+
+
